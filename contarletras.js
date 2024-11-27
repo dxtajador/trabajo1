5b5ff25089521ea41c7d62eeed92ad0bd3a4c738
joshoa trejo <joshoat100@gmail.com>
2024-11-26 22:14:43 -0800

    Add files via upload

---
+++ contarletras.js
@@ -0,0 +1,7 @@
+// Nombre completo
+let nombre = "joshoa alexei trejo barragan";
+
+// Eliminar espacios y contar las letras
+let cantidadLetras = nombre.replace(/ /g, '').length;
+
+console.log("Cantidad de letras en el nombre:", cantidadLetras);
